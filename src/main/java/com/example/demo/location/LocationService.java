74f88a17c971e83fa84c172b0cc0194e4e4148db
aneja <aneja.pasalic@gmail.com>
2024-07-12 11:19:37 +0200

    added deploy.yml

--- src/main/java/com/example/demo/location/LocationService.java
+++ src/main/java/com/example/demo/location/LocationService.java
@@ -2,20 +2,21 @@ package com.example.demo.location;
 
 import com.example.demo.location.client.OpenCageClient;
 import com.example.demo.location.client.dto.LocationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class LocationService {
+    private final Logger logger = LoggerFactory.getLogger(LocationService.class.getName());
    private final OpenCageClient openCageClient;
-
 
     public LocationService(OpenCageClient openCageClient) {
         this.openCageClient = openCageClient;
     }
 
     public LocationResponse getLocation(String name) {
+        logger.info("Getting coordinates for location: " + name);
         return openCageClient.getCoordinates(name);
     }
 }
